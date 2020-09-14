cf97f42bf63433c208fea3f1a66351d0a8a4bf39
noqnakhier <noqnakhier@gmail.com>
2020-09-14 17:36:45 +0800

    Fonts settigns

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cfs--fontsize-steps (quote (4 4 4)) t)
- '(package-selected-packages (quote (## cnfonts chinese-fonts-setup fullframe))))
+ '(package-selected-packages (quote (evil ## cnfonts chinese-fonts-setup fullframe))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
